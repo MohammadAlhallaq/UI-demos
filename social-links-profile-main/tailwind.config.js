87130a3c6600f33bf79081a0f0c7d89e48c380e6
MohammadAlhallaq <mohammad.alhallaq1@gmail.com>
2025-05-17 21:54:40 +0300

    init

---
+++ social-links-profile-main/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./**/*.html"], // Point to your HTML files
+    corePlugins: {
+        preflight: false, // disables Tailwind's CSS reset
+    },
+
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
